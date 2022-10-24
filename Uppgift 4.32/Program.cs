ad1f52d25958c97723c5ce8838851e2e1308c6f3
E-Svensson <edvin.sven123@gmail.com>
2022-10-24 21:08:33 +0200

    Add project files.

---
+++ Uppgift 4.32/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bestäm kvadraternas sidlängd: "); int Sidlängd = int.Parse(Console.ReadLine());
+            Console.Write("Bestäm rutnätets bredd: "); int Bredd = int.Parse(Console.ReadLine());
+            Console.Write("Bestäm rutnätets höjd: "); int Höjd = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < Höjd; i++)
+            {
+                for (int j = 0; j < Sidlängd; j++)
+                {
+                    for (int k = 0; k < Bredd; k++)
+                    {
+                        Console.Write("***** ");
+
+                    }
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
